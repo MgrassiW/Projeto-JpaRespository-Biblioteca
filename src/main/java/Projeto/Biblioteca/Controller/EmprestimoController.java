0d94b9bc06e24f8e9679028c197ece1892eee8df
Marco <marcooo0034h@gmail.com>
2026-09-13 22:58:51 -0300

    Feat : Implementa a classe emprestimoController

---
+++ src/main/java/Projeto/Biblioteca/Controller/EmprestimoController.java
@@ -0,0 +1,51 @@
+package Projeto.Biblioteca.Controller;
+
+import Projeto.Biblioteca.DTO.EmprestimoRequest;
+import Projeto.Biblioteca.DTO.EmprestimoResponse;
+import Projeto.Biblioteca.DTO.UsuarioRequest;
+import Projeto.Biblioteca.DTO.UsuarioResponse;
+import Projeto.Biblioteca.Service.EmprestimosService;
+import Projeto.Biblioteca.Service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/emprestimos")
+public class EmprestimoController {
+
+    private final EmprestimosService emprestimosService;
+
+
+    @PostMapping
+    public EmprestimoResponse save(@RequestBody EmprestimoRequest request) {
+
+        return emprestimosService.save(request);
+    }
+
+    @GetMapping
+    public List<EmprestimoResponse> findAll() {
+
+        return emprestimosService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public EmprestimoResponse findById(@PathVariable Long id) {
+
+        return emprestimosService.findById(id);
+    }
+
+    @PutMapping("/{usuario}/{id}")
+    public EmprestimoResponse update(@RequestBody EmprestimoRequest request , @PathVariable Long id) {
+
+        return emprestimosService.update(request , id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+        emprestimosService.delete(id);
+    }
+}
