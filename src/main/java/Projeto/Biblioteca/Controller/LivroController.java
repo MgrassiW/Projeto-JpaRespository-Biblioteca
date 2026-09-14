ce237efd08456260660cb76e66eef9f9e0f65596
Marco <marcooo0034h@gmail.com>
2026-09-13 22:57:01 -0300

    Feat: Implementa a classe LivroController

---
+++ src/main/java/Projeto/Biblioteca/Controller/LivroController.java
@@ -0,0 +1,52 @@
+package Projeto.Biblioteca.Controller;
+
+
+import Projeto.Biblioteca.DTO.LivroRequest;
+import Projeto.Biblioteca.DTO.LivroResponse;
+import Projeto.Biblioteca.DTO.UsuarioRequest;
+import Projeto.Biblioteca.DTO.UsuarioResponse;
+import Projeto.Biblioteca.Service.LivroService;
+import Projeto.Biblioteca.Service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    private final LivroService livroService;
+
+
+    @PostMapping
+    public LivroResponse save(@RequestBody LivroRequest request) {
+
+        return livroService.save(request);
+    }
+
+    @GetMapping
+    public List<LivroResponse> findAll() {
+
+        return livroService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LivroResponse findById(@PathVariable Long id) {
+
+        return livroService.findById(id);
+    }
+
+    @PutMapping("/{usuario}/{id}")
+    public LivroResponse update(@RequestBody LivroRequest request , @PathVariable Long id) {
+
+        return livroService.update(request , id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+        livroService.delete(id);
+    }
+}
