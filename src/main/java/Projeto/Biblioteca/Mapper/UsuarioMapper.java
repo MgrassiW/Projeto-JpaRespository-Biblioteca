b789c66f84c9e45ceb5298ad8493c1001d24f006
Marco <marcooo0034h@gmail.com>
2026-09-13 22:31:04 -0300

    Feat: Implementa a classe service de Usuario

--- src/main/java/Projeto/Biblioteca/Mapper/UsuarioMapper.java
+++ src/main/java/Projeto/Biblioteca/Mapper/UsuarioMapper.java
@@ -1,4 +1,32 @@
 package Projeto.Biblioteca.Mapper;
 
+import Projeto.Biblioteca.DTO.UsuarioRequest;
+import Projeto.Biblioteca.DTO.UsuarioResponse;
+import Projeto.Biblioteca.Entity.Usuario;
+
 public class UsuarioMapper {
+
+    public Usuario toEntity(UsuarioRequest request) {
+
+        return Usuario.builder()
+                .nome(request.nome())
+                .email(request.email())
+                .build();
+    }
+
+    public UsuarioResponse toResponse(Usuario usuario) {
+
+        return new UsuarioResponse(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail()
+        );
+    }
+
+    public void updateEntity(UsuarioRequest request , Usuario usuario) {
+
+        usuario.setNome(request.nome());
+        usuario.setEmail(request.email());
+
+    }
 }
